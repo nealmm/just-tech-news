4c92e0b142b0098357757000f88b116fe048dfa1
Matt Neal <neal.matt.m@gmail.com>
2022-08-23 21:00:01 -0400

    helper functions

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
